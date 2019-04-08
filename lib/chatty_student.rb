0e6d21d662da33f3f678acd88292ca95d8a15116
ferrisboran <ferris@ferryrules.com>
2019-04-07 22:20:06 -0400

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,11 +1,15 @@
-class ChattyStudent
+class ChattyStudent < Student
 
   def hello
-   puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    print super
+    print "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
 
   def raise_hand
-    puts "Pick me!\n" * 10
+    for i in 1..10 do
+      super
+      i += 1
+    end
   end
 
 end
